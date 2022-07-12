dbaa47fdc232df86273dceebe0ba513741f0712f
yuanhu.li <yuanhu.li@hand-china.com>
2022-07-12 10:52:05 +0800

    feat: 新增自定义hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "./utils/index";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const inputValue = useDebounce(value, 500);
+  useEffect(() => {
+    console.log(inputValue, "变化了");
+  }, [inputValue]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {inputValue}
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target?.value)}
+      />
     </div>
   );
 }
