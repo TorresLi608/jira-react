d1ff0fc4c752a9a72a4f7fc6d0de2dc56b0ccfce
yuanhu.li <yuanhu.li@hand-china.com>
2022-07-12 15:11:11 +0800

    feat: 新建目录提交

---
+++ src/utils/index.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export const useDebounce = <T>(val: T, delay?: number) => {
+  const [value, setValue] = useState<T>(val);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setValue(val);
+    }, delay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [val, delay]);
+  return value;
+};
+
+export const useArray = <T>(val: T[]) => {
+  const [value, setValue] = useState<T[]>(val);
+  const add = (data: T) => {
+    const arr = [...value, data];
+    setValue(arr);
+  };
+  const clear = () => {
+    setValue([]);
+  };
+
+  const removeIndex = (index: number) => {
+    value.splice(index, 1);
+    console.log(value, "val");
+    setValue([...value]);
+  };
+
+  return {
+    value,
+    add,
+    clear,
+    removeIndex,
+  };
+};
